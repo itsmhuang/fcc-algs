b27ad0637d1e2111aa21c638697e625b50f65b28
Michelle Huang <mhuangsmailbox@gmail.com>
2016-05-18 14:30:49 -0700

    slice or splice method

--- slasher.js
+++ slasher.js
@@ -1,7 +1,10 @@
 function slasher(arr, howMany) {  
   arr = arr.slice(howMany);
   return arr;
-  
+
+	//with splice
+	//arr = arr.splice(howMany);
+	//return arr; 
 }
 
 //slasher([1, 2, 3], 2);// should return [3].
