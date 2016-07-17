51845646396135eec39427fe63f29491fb660f9e
Michelle Huang <mhuangsmailbox@gmail.com>
2016-07-16 21:22:55 -0700

    add forEach solution

--- findLongestWord.js
+++ findLongestWord.js
@@ -2,11 +2,22 @@ function findLongestWord(str) {
   //place words into array
   var arr = str.split(' ');
   var long = arr[0];
+  
+  arr.forEach(function(e){
+    if(e.length > long.length) {
+      long = e;
+    }
+  });
+
+  /*
+  or a for loop
   for(var i = 0; i < arr.length; i++) {
     if(arr[i].length > long.length) {
       long = arr[i];
     } 
   }
+*/
+
   return long.length;
 }
 
