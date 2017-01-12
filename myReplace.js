106d7e4e654686b0a597c2f521774612ddc921c1
Michelle Huang <mhuangsmailbox@gmail.com>
2017-01-11 18:34:39 -0800

    add comments

--- myReplace.js
+++ myReplace.js
@@ -1,13 +1,18 @@
 function myReplace(str, before, after) {
   var newStr;
   var capsAfter;
+  //if before var is capitalized
   if((/[A-Z]/.test(before[0]))){
+    //capitalize after var
     capsAfter = after.split("");
     capsAfter[0] = capsAfter[0].toUpperCase();
     capsAfter = capsAfter.join("");
+    //use capitalized after (capsAfter) in .replace()
     newStr = str.replace(before,capsAfter);
   }
+  //if before var not capitalized
   else{
+    //use default before and after vars
     newStr = str.replace(before,after);  
   }
   console.log(newStr);
