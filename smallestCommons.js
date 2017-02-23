9ad21a1674c8567a7d0befbb956b844bc4dd16a8
Michelle Huang <mhuangsmailbox@gmail.com>
2017-02-22 21:12:24 -0800

    smallest common multiple in a range of numbers

---
+++ smallestCommons.js
@@ -0,0 +1,33 @@
+function smallestCommons(arr) {
+  var multiplier = 1;
+  //sort array small > large
+  var sortedArr = arr.sort(function(a,b){
+    return a -b;
+  });  
+  var smallest = sortedArr[0];
+  var largest = sortedArr[1];
+  //multiple never changes 
+  var multiple = arr[0]*arr[1];  
+  //will change to get new multiples to check
+  var multipleOfMultiple = arr[0]*arr[1];  
+   for(var i= smallest;i<=largest;i++){
+     //if i is not multiple
+     if(multipleOfMultiple % i !== 0){
+       //increment multiplier
+       multiplier++;
+       //check new multiple
+       multipleOfMultiple = multiplier * multiple;
+       //i goes back down to smallest
+       i = smallest;
+      }
+    }
+ 
+  return multipleOfMultiple;
+  
+}
+
+//take the larger number and find the first multiple of it
+//test whether that multiple is divisible by the numbers up to that largest num
+// largest % (each number in the range) === 0
+
+smallestCommons([1,5]);
